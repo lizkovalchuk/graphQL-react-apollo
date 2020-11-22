9362158f1ddf5629f56e4998c58ded1dcefa909e
Liz Kovalchuk <liz.kovalchuk@shopify.com>
2020-11-22 18:50:24 -0500

    mutation added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import CreateLink from './CreateLink'
 import LinkList from './LinkList'
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return (
+      <>
+        <LinkList />
+        <CreateLink />
+      </>
+    )
   }
 }
 
